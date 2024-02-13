86206c053f0048537cba89fc6d818bbbb223193c
iam8uman <me8848you@gmail.com>
2024-02-13 21:09:25 +0545

    Responsive Navbar completed

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,13 +5,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { hostname: "source.unsplash.com" },
-      { hostname: "plus.unsplash.com" },
+      { hostname: "images.unsplash.com" },
       { hostname: "cdn.pixabay.com" },
       { hostname: "i.ibb.co" },
       { hostname: "images.pexels.com" },
-      { hostname: "images.unsplash.com" },
-      { hostname: "www.pexels.com" },
-      { hostname: "overreacted.io" },
+      { hostname: "unsplash.com" },
+      { hostname: "plus.unsplash.com" },
     ],
   },
 };
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
-import { LandinngPageOne } from "../components/LandingPage";
+import {  Navbar } from "@/components/Navbar";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <LandinngPageOne />
+      <Navbar />
+      <Button 
+      > Hello world</Button>
     </>
   );
 }
