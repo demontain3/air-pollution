fb4f12e18d44457d568b6a0eef9748015132934c
karkiamrit <amrit.191503@ncit.edu.np>
2024-02-17 15:10:59 +0545

    auth service and controller

--- backend/apps/auth/src/auth.controller.ts
+++ backend/apps/auth/src/auth.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { CurrentUser, JwtAuthGuard, User } from '@app/common';
+import { Response } from 'express';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.authService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(
+    @CurrentUser() user : User,
+    @Res({ passthrough:true }) response: Response
+  ){
+    const jwt = await this.authService.login(user, response);
+    response.send(jwt);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @MessagePattern('authenticate')
+  async authenticate(@Payload() data:any){
+    return data.user
   }
 }
