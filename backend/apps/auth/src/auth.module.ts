27da30afe0c7a7df69678f4dfa93ac0e524259b6
karkiamrit <amrit.191503@ncit.edu.np>
2024-02-17 13:23:54 +0545

    postgres and typeorm setup

--- backend/apps/auth/src/auth.module.ts
+++ backend/apps/auth/src/auth.module.ts
@@ -4,12 +4,18 @@ import { AuthService } from './auth.service';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { UsersModule } from './users/users.module';
 import { User } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    DatabaseModule,
     DatabaseModule.forFeature([User]),
     LoggerModule,
     UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'apps/auth/.env',
+    }),
 ],
   controllers: [AuthController],
   providers: [AuthService],
