bd61887c43c85725c54ff61b1e0ac35b0a4d1274
iam8uman <me8848you@gmail.com>
2024-04-05 00:04:32 +0545

    Basic langing page almost complete

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,12 +5,16 @@ import Hero from "@/components/Hero/Hero"
 import { Navbar } from "@/components/Navbar"
 import About from "@/components/about/About"
 import Feature from "@/components/Feature"
+import Pricing from "@/components/pricing/Pricing"
+import Movingcard from "@/components/card/Movingcard"
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Feature />
+      <Pricing />
+      <Movingcard />
       <About />
     </>
   )
---
+++ frontend/src/components/card/Movingcard.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+import tonyning from "@/../public/business-woman.png";
+import shrutidua from "@/../public/business-woman.png";
+import samvalji from "@/../public/business-woman.png";
+import avatar from "@/../public/business-woman.png";
+
+export default function MovingCard() {
+  return (
+    <>
+      <div className=" mx-auto text-center  mt-10">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl lg:text-5xl inline-block aaa">
+          What people say about us! <br />{" "}
+        </h2>
+          <p className="text-xl mt-6"> Over 1000+ people trust us.</p>
+      </div>
+      <div className=" rounded-md flex flex-col antialiased  items-center justify-center  overflow-hidden my-10 lg:my-12">
+        <div>
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+            // className="pt-10"
+          />
+        </div>
+
+        <div className="">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="left"
+            speed="slow"
+            // className="pb-10"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+const testimonials = [
+  {
+    quote:
+      "SixDesign.ca delivers exceptional web design, exceeding expectations with professionalism, creativity, and seamless communication. Highly recommended for outstanding results.",
+    name: "Tony Ning",
+    title: "Real Estate Broker",
+    img: tonyning,
+  },
+  {
+    quote:
+      "Transformative web design services by SixDesign.ca redefine online presence. With creativity, professionalism, and seamless communication, they ensure remarkable results. Highly recommended.",
+    name: "Shruti Dua",
+    title: "Sales Representive",
+    img: shrutidua,
+  },
+  {
+    quote:
+      "Elevate your online presence with exceptional web design services from SixDesign.ca. Experience creativity, professionalism, and seamless communication for remarkable results.",
+    name: "Sam Valji",
+    title: "Real Estate Broker",
+    img: samvalji,
+  },
+  {
+    quote:
+      "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
+    name: "Jane Austen",
+    title: "CEO, SixDesign.ca",
+    img: avatar,
+  },
+  {
+    quote:
+      "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
+    name: "Herman Melville",
+    title: "CEO, Garima Bikas Bank",
+    img: avatar,
+  },
+];
