085742df87465538089f23a9657d2aaa8852dd93
iam8uman <me8848you@gmail.com>
2024-04-13 10:00:55 +0545

    Dashboard main dashboard is working

--- frontend/src/app/(dashboard)/admindash/layout.tsx
+++ frontend/src/app/(dashboard)/admindash/layout.tsx
@@ -1,14 +1,24 @@
-export default function RootLayout({
+import Header from "@/components/layout/header";
+import Sidebar from "@/components/layout/sidebar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Next Shadcn Dashboard Starter",
+  description: "Basic dashboard with Next.js and Shadcn",
+};
+
+export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        {/* Layout UI */}
-        <main>{children}</main>
-      </body>
-    </html>
-  )
+    <>
+      <Header />
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar />
+        <main className="w-full pt-16">{children}</main>
+      </div>
+    </>
+  );
 }
--- frontend/src/app/(dashboard)/admindash/page.tsx
+++ frontend/src/app/(dashboard)/admindash/page.tsx
@@ -1,170 +1,165 @@
-// import Link from "next/link"
-// import {
-//   Bell,
-//   CircleUser,
-//   Home,
-//   LineChart,
-//   Menu,
-//   Package,
-//   Package2,
-//   Search,
-//   ShoppingCart,
-//   Users,
-// } from "lucide-react"
+// import { CalendarDateRangePicker } from "@/components/date-range-picker";
+import { Overview } from "@/components/overview";
+import { RecentSales } from "@/components/recent-sales";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
-// import { Badge } from "@/components/ui/badge"
-// import { Button } from "@/components/ui/button"
-// import {
-//   Card,
-//   CardContent,
-//   CardDescription,
-//   CardHeader,
-//   CardTitle,
-// } from "@/components/ui/card"
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuLabel,
-//   DropdownMenuSeparator,
-//   DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu"
-// import { Input } from "@/components/ui/input"
-// import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-// import Adminsidebar from "@/components/layout/adminsidebar"
-
-// export default function Sidebar() {
-//   return (
-//     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-//       <Adminsidebar />
-//       <div className="flex flex-col">
-//         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-//           <Sheet>
-//             <SheetTrigger asChild>
-//               <Button
-//                 variant="outline"
-//                 size="icon"
-//                 className="shrink-0 md:hidden"
-//               >
-//                 <Menu className="h-5 w-5" />
-//                 <span className="sr-only">Toggle navigation menu</span>
-//               </Button>
-//             </SheetTrigger>
-//             <SheetContent side="left" className="flex flex-col">
-//               <nav className="grid gap-2 text-lg font-medium">
-//                 <Link
-//                   href="#"
-//                   className="flex items-center gap-2 text-lg font-semibold"
-//                 >
-//                   <Package2 className="h-6 w-6" />
-//                   <span className="sr-only">Acme Inc</span>
-//                 </Link>
-//                 <Link
-//                   href="#"
-//                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-//                 >
-//                   <Home className="h-5 w-5" />
-//                   Dashboard
-//                 </Link>
-//                 <Link
-//                   href="#"
-//                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-//                 >
-//                   <ShoppingCart className="h-5 w-5" />
-//                   Orders
-//                   <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-//                     6
-//                   </Badge>
-//                 </Link>
-//                 <Link
-//                   href="#"
-//                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-//                 >
-//                   <Package className="h-5 w-5" />
-//                   Products
-//                 </Link>
-//                 <Link
-//                   href="#"
-//                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-//                 >
-//                   <Users className="h-5 w-5" />
-//                   Customers
-//                 </Link>
-//                 <Link
-//                   href="#"
-//                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
-//                 >
-//                   <LineChart className="h-5 w-5" />
-//                   Analytics
-//                 </Link>
-//               </nav>
-//               <div className="mt-auto">
-//                 <Card>
-//                   <CardHeader>
-//                     <CardTitle>Upgrade to Pro</CardTitle>
-//                     <CardDescription>
-//                       Unlock all features and get unlimited access to our
-//                       support team.
-//                     </CardDescription>
-//                   </CardHeader>
-//                   <CardContent>
-//                     <Button size="sm" className="w-full">
-//                       Upgrade
-//                     </Button>
-//                   </CardContent>
-//                 </Card>
-//               </div>
-//             </SheetContent>
-//           </Sheet>
-//           <div className="w-full flex-1">
-//             <form>
-//               <div className="relative">
-//                 <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-//                 <Input
-//                   type="search"
-//                   placeholder="Search products..."
-//                   className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-//                 />
-//               </div>
-//             </form>
-//           </div>
-//           <DropdownMenu>
-//             <DropdownMenuTrigger asChild>
-//               <Button variant="secondary" size="icon" className="rounded-full">
-//                 <CircleUser className="h-5 w-5" />
-//                 <span className="sr-only">Toggle user menu</span>
-//               </Button>
-//             </DropdownMenuTrigger>
-//             <DropdownMenuContent align="end">
-//               <DropdownMenuLabel>My Account</DropdownMenuLabel>
-//               <DropdownMenuSeparator />
-//               <DropdownMenuItem>Settings</DropdownMenuItem>
-//               <DropdownMenuItem>Support</DropdownMenuItem>
-//               <DropdownMenuSeparator />
-//               <DropdownMenuItem>Logout</DropdownMenuItem>
-//             </DropdownMenuContent>
-//           </DropdownMenu>
-//         </header>
-//         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-//           <div className="flex items-center">
-//             <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
-//           </div>
-//           <div
-//             className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-//             x-chunk="dashboard-02-chunk-1"
-//           >
-//             <div className="flex flex-col items-center gap-1 text-center">
-//               <h3 className="text-2xl font-bold tracking-tight">
-//                 You have no products
-//               </h3>
-//               <p className="text-sm text-muted-foreground">
-//                 You can start selling as soon as you add a product.
-//               </p>
-//               <Button className="mt-4">Add Product</Button>
-//             </div>
-//           </div>
-//         </main>
-//       </div>
-//     </div>
-//   )
-// }
+export default function page() {
+  return (
+    <ScrollArea className="h-full">
+      <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+        <div className="flex items-center justify-between space-y-2">
+          <h2 className="text-3xl font-bold tracking-tight">
+            Hi, Welcome back 👋
+          </h2>
+          <div className="hidden md:flex items-center space-x-2">
+            {/* <CalendarDateRangePicker /> */}
+            <Button>Download</Button>
+          </div>
+        </div>
+        <Tabs defaultValue="overview" className="space-y-4">
+          <TabsList>
+            <TabsTrigger value="overview">Overview</TabsTrigger>
+            <TabsTrigger value="analytics" disabled>
+              Analytics
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="overview" className="space-y-4">
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Total Revenue
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">$45,231.89</div>
+                  <p className="text-xs text-muted-foreground">
+                    +20.1% from last month
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Subscriptions
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                    <circle cx="9" cy="7" r="4" />
+                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+2350</div>
+                  <p className="text-xs text-muted-foreground">
+                    +180.1% from last month
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">Sales</CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <rect width="20" height="14" x="2" y="5" rx="2" />
+                    <path d="M2 10h20" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+12,234</div>
+                  <p className="text-xs text-muted-foreground">
+                    +19% from last month
+                  </p>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                  <CardTitle className="text-sm font-medium">
+                    Active Now
+                  </CardTitle>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="h-4 w-4 text-muted-foreground"
+                  >
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                  </svg>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-2xl font-bold">+573</div>
+                  <p className="text-xs text-muted-foreground">
+                    +201 since last hour
+                  </p>
+                </CardContent>
+              </Card>
+            </div>
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-7">
+              <Card className="col-span-4">
+                <CardHeader>
+                  <CardTitle>Overview</CardTitle>
+                </CardHeader>
+                <CardContent className="pl-2">
+                  <Overview />
+                </CardContent>
+              </Card>
+              <Card className="col-span-4 md:col-span-3">
+                <CardHeader>
+                  <CardTitle>Recent Sales</CardTitle>
+                  <CardDescription>
+                    You made 265 sales this month.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <RecentSales />
+                </CardContent>
+              </Card>
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </ScrollArea>
+  );
+}
