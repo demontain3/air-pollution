d70076b758786992a94976a46403352193b1d605
Suman Sharma <suman@192.168.1.12>
2024-05-28 21:38:06 +0545

    mapping of heatmap data on map with colors

--- frontend/src/app/(dashboard)/admindash/map/page.tsx
+++ frontend/src/app/(dashboard)/admindash/map/page.tsx
@@ -1,5 +1,9 @@
+"use client"
+
 import React from "react"
 import dynamic from "next/dynamic"
+import { ChevronLeft } from "lucide-react"
+import { useRouter } from "next/navigation"
 
 const OSMap = dynamic(() => import("@/components/Map/OSMap"), {
   ssr: false,
@@ -15,8 +19,20 @@ const page = () => {
   //   },
   // }
 
+  const router = useRouter()
+
   return (
     <div className="text-slate-300">
+       <div className="flex flex-row items-center gap-2 text-3xl font-extrabold text-primary my-6">
+          <ChevronLeft
+            size={28}
+            strokeWidth={3}
+            onClick={() => router.back()}
+            className="cursor-pointer text-primary transition-transform duration-300 ease-in-out hover:scale-110 hover:text-green-400"
+          />
+          <h1>Routes</h1>
+        </div>
+
       <OSMap  />
     </div>
   )
